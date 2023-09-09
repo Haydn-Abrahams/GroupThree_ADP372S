111b0d7d8ef53d2aa30acceda1bdf42ab118cb73
Haydn-Abrahams <abrahamshaydn@gmail.com>
2023-09-09 18:10:34 +0200

    Repository pathway update.

--- src/main/java/za/ac/cput/repository/ICustomerRepository.java
+++ src/main/java/za/ac/cput/repository/ICustomerRepository.java
@@ -1,10 +1,10 @@
-package za.ac.cput.respository;
+package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Customer;
 
 @Repository
-public interface IEmployeeRepository extends JpaRepository<Employee,String> {
+public interface ICustomerRepository extends JpaRepository<Customer,String> {
 
 }
