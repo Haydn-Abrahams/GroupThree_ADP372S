3df49d47ef65b00d2a599f2ffc2baa6c69929478
Haydn-Abrahams <abrahamshaydn@gmail.com>
2023-09-09 18:16:40 +0200

    PayrollFactoryTest.

---
+++ src/test/java/za/ac/cput/factory/PayrollFactoryTest.java
@@ -0,0 +1,18 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Payroll;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PayrollFactoryTest {
+
+    @Test
+    void buildPayroll() {
+        Payroll payroll1 =  PayrollFactory.buildPayroll("","Detailer", 23, 2, 90,16830.00);
+        System.out.println(payroll1.toString());
+        assertNotNull(payroll1);
+    }
+
+
+}
