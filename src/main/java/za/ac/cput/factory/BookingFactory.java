48a4f8c71271d14663b5590383e2dbaecfce62eb
Ziyaad <ziyaadh63@gmail.com>
2023-09-10 12:27:05 +0200

    Changes made in Booking and BookingFactory classes

--- src/main/java/za/ac/cput/factory/BookingFactory.java
+++ src/main/java/za/ac/cput/factory/BookingFactory.java
@@ -1,15 +1,16 @@
 package za.ac.cput.factory;
 
 import za.ac.cput.domain.Booking;
-import za.ac.cput.domain.Customer;
 import za.ac.cput.util.Helper;
 
 import java.sql.Time;
 import java.util.Date;
 
+import static java.lang.String.*;
+
 public class BookingFactory {
-    public static Booking buildBooking(Date date, Time time, Customer customerId){
-        if(Helper.isNullorEmpty(String.valueOf(date)) || Helper.isNullorEmpty(String.valueOf(time) || Helper.isNullorEmpty(customerId))){
+    public static Booking buildBooking(Date date, Time time, String customerId){
+        if(Helper.isNullorEmpty(String.valueOf(date)) || Helper.isNullorEmpty(String.valueOf(time)) || Helper.isNullorEmpty(customerId)){
             return null;
         }
 
