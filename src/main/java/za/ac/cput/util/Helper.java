7ff0bd74f201217c19047da079889e4da3abb79f
Aqu38080 <willliamsaquilla70@gmail.com>
2023-09-11 12:43:12 +0200

    this is my pojo classes , factory + test factory repo classes my service class controller + test controller and test service update helper class

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -7,6 +7,19 @@ import java.util.UUID;
 
 public class Helper {
 
+    public static boolean isNullOrEmpty(String s){
+
+        if (s == null || s.isEmpty()){
+            return true;
+        }
+        return false;
+
+    }
+
+    public static String generateId(){
+        return UUID.randomUUID().toString();
+    }
+
     public static boolean isNullorEmpty(String i) {
         return (i == null || i.isEmpty() || i == " ");
 
